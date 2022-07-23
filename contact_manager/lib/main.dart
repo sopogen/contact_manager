cac57b3eb8f88edb783e5083b3719f9011a10c5e
sopogen <spacefor555@gmail.com>
2022-07-23 18:03:13 +0900

    chore: test main theme

--- contact_manager/lib/main.dart
+++ contact_manager/lib/main.dart
@@ -8,12 +8,20 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(),
-        bottomNavigationBar: BottomAppBar(),
-        )
-    );
-  } 
+        home: Scaffold(
+            appBar: AppBar(title: Text("앱임")),
+            body: Text('안녕'),
+            bottomNavigationBar: BottomAppBar(
+                child: SizedBox(
+              height: 70,
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Icon(Icons.call),
+                    Icon(Icons.message),
+                    Icon(Icons.book)
+                  ]),
+            ))));
+  }
 }
