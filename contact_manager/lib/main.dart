7e0b6afbd44d2d873e6a5313a4e094d1b5e23d71
sopogen <spacefor555@gmail.com>
2022-07-25 23:38:12 +0900

    Chore: align appbar

--- contact_manager/lib/main.dart
+++ contact_manager/lib/main.dart
@@ -9,19 +9,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(title: Text("앱")),
-            body: Align(
-              alignment: Alignment.topCenter,
-              child: Container(
-                  width: double.infinity,
-                  height: 50,
-                  margin: EdgeInsets.all(5),
-                  padding: EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black),
-                      color: Colors.blue),
-                  child: Text('test')),
-            )));
+        home: Scaffold(appBar: AppBar(actions: [Icon(Icons.star)])));
   }
 }
